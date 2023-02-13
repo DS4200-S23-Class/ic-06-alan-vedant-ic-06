993b98605318354cf2ebfdc46b20000aec5f9d12
vbhagat02 <vedant.bhagat0204@gmail.com>
2023-02-13 15:18:53 -0500

    changes

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeColor() {
+
+    document.getElementById("line").style.backgroundColor = '#911';
+
+}
+
+document.getElementById("colorButton").addEventListener('click', changeColor);
